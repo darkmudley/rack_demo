b4bc98e17641949787ca88dc6adde54c59e3fb89
tom <tommaso@pavese.me>
2016-02-16 02:28:51 +0000

    added a Dog model, using it in the controllers

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,14 +1,17 @@
 require './app/controllers/base_controller'
+require './app/models/dog'
 
 class DogsController < BaseController
   def index
     @title = "This is Dogs#index"
+    @dogs = Dog.all
     build_response 200, render_template(:index)
   end
 
 
   def show
     @title = "This is Dogs#show - id: #{params[:id]}"
+    @dog = Dog.find(params[:id])
     build_response 200, render_template(:show)
   end
 end
