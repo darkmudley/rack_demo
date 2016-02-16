57f3bff5e6c03bd137f7e63898b94b9d5811ca3e
tom <tommaso@pavese.me>
2016-02-16 02:31:19 +0000

    extracted generic functionality in a base model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,45 +1,9 @@
-require 'pstore'
+require './app/models/base'
 
-class Dog
-  class << self
-    def find(id)
-      db.transaction(true) do
-        db[db_id(id)]
-      end
-    end
-
-    def all
-      db.transaction(true) do
-        db.roots.map { |key| db[key] }
-      end
-    end
-
-    def db
-      @db ||= PStore.new("db.pstore")
-    end
-
-    def db_id(base)
-      "#{self.name}_#{base}"
-    end
-  end
-
-
+class Dog < Base
   attr_accessor :id, :name
-
 
   def initialize(*args)
     @id, @name = args
-  end
-
-
-  def save
-    self.class.db.transaction do |db|
-      db[db_id] = self
-    end
-  end
-
-
-  def db_id
-    self.class.db_id(id)
   end
 end
