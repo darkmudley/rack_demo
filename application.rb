c6586495b54ed4b75001f7244f61fd773962a066
tom <tommaso@pavese.me>
2016-06-14 01:48:16 +0200

    no need to return 404 for the favicon in the application object

--- application.rb
+++ application.rb
@@ -4,14 +4,8 @@ Dir.glob(app_files).each { |file| require(file) }
 class Application
   def call(env)
     request = Rack::Request.new(env)
-
-    if request.path =~ /favicon/
-      return [404, {}, ["No Favicons here"]]
-    end
-
     serve_request(request)
   end
-
 
   def serve_request(request)
     Router.new(request).process!
