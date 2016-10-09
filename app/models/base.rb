4c4b72322ebcedd3f4aef6143c7439a4e3e60bb5
tom <tommaso@pavese.me>
2016-10-09 18:43:56 +0100

    refactored the base model methods, fixed bugs

--- app/models/base.rb
+++ app/models/base.rb
@@ -2,36 +2,68 @@ require 'pstore'
 
 class Base
   module ClassMethods
+
+    # Find a record by ID
+    #
     def find(id)
       db.transaction(true) do
-        db[db_id(id)]
+        db[derive_db_id(self.name, id)]
       end
     end
 
+    # Returns all records for this model
+    #
     def all
       db.transaction(true) do
-        db.roots
-          .select { |key| key.start_with?(self.name) }
-          .map { |key| db[key] }
+        ids = extract_model_ids(db)
+        ids.map { |key| db[key] }
       end
     end
+
+    # Store an instance in the DB
+    #
+    def save(object)
+      db_id = derive_db_id(object.class.name, object.id)
+      db.transaction do
+        db[db_id] = object
+      end
+    end
+
+    # Scoped by class, to auto-increment the IDs
+    #
+    def next_available_id
+      last_id = all_ids.map do |key|
+        key.sub("#{self.name}_", "").to_i
+      end.max.to_i
+
+      last_id + 1
+    end
+
+    private
 
     def db
       @db ||= PStore.new("db.pstore")
     end
 
-    def db_id(base)
-      "#{self.name}_#{base}"
+    # Scoped by class, so that different model classes
+    # can have the same numerical IDs
+    #
+    def derive_db_id(model_name, obj_id)
+      "#{model_name}_#{obj_id}"
     end
 
+    # All the IDs for this model
+    #
     def all_ids
-      db.transaction(true) { |db| db.roots }
+      db.transaction(true) do |db|
+        extract_model_ids(db)
+      end
     end
 
-    def next_available_id
-      all_ids.map do |key|
-        key.sub(/[A-Za-z]+_/, '').to_i
-      end.max + 1
+    def extract_model_ids(store)
+      store.roots.select do |key|
+        key.start_with?(self.name)
+      end
     end
   end
   extend ClassMethods
@@ -39,14 +71,7 @@ class Base
 
   def save
     ensure_id
-    self.class.db.transaction do |db|
-      db[db_id] = self
-    end
-  end
-
-
-  def db_id
-    self.class.db_id(id)
+    self.class.save(self)
   end
 
 
