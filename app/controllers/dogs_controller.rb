3ea59a40b6f562ed0d2954a2b3d8d19360b96b3b
tom <tommaso@pavese.me>
2016-10-10 01:39:57 +0100

    small updated to the dogs controller

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,21 +2,21 @@ require_relative './base_controller.rb'
 
 class DogsController < BaseController
   def index
-    @title = "This is Dogs#index"
+    @title = "So many dogs"
     @dogs = Dog.all
     build_response render_template
   end
 
 
   def show
-    @title = "This is Dogs#show - id: #{params[:id]}"
     @dog = Dog.find(params[:id])
+    @title = "#{@dog.name}'s page"
     build_response render_template
   end
 
 
   def new
-    @title = "Create e new dog"
+    @title = "More dogs please"
     build_response render_template
   end
 
@@ -24,7 +24,6 @@ class DogsController < BaseController
   def create
     dog = Dog.new(name: params['dog']['name'])
     dog.save
-
     redirect_to "dogs/#{dog.id}"
   end
 end
